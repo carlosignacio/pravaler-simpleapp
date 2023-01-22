6d21009f0bfe750faa5519cc35c69ec9b97e6fc0
Carlos Eduardo Ignacio <cadardo@hotmail.com>
2023-01-22 17:31:15 -0300

    ajuste alias

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,7 +2,7 @@
 # Network Configuration
 #=============================================================================
 module "network_config" {
-  source = "/home/cme/certi/terraform-modules/modules/network"
+  source = "terraform/terraform-modules/modules/network"
   
   project_id                  = var.project_id
   region                      = var.region
@@ -12,7 +12,7 @@ module "network_config" {
 #=============================================================================#
 # Cluster GKE Configuration
 module "cluster_gke" {
-  source                      = "/home/cme/certi/terraform-modules/modules/cluster"
+  source                      = "terraform/terraform-modules/modules/cluster"
 
   cluster_name                = var.cluster_name
   region                      = var.region
@@ -25,7 +25,7 @@ module "cluster_gke" {
 }
 
 module "microservices_node_pool" {
-  source                      = "/home/cme/certi/terraform-modules/modules/node_pool"
+  source                      = "terraform/terraform-modules/modules/node_pool"
 
   pool_name                   = "microservices"
   cluster_name                = var.cluster_name
@@ -43,7 +43,7 @@ module "microservices_node_pool" {
 }
 
 module "satelite_node_pool" {
-  source                      = "/home/cme/certi/terraform-modules/modules/node_pool"
+  source                      = "terraform/terraform-modules/modules/node_pool"
 
   pool_name                   = "satelite"
   cluster_name                = var.cluster_name
@@ -54,77 +54,8 @@ module "satelite_node_pool" {
   min_nodes                   = var.min_nodes
   machine_type                = "n2-standard-4"
   #machine_type                = "t2a-standard-1"
-  label_name                  = "certi-satelite-np"
-  label_node                  = "certi-satelite-np"
+  label_name                  = "pravaler-satelite-np"
+  label_node                  = "pravaler-satelite-np"
 
   depends_on                  = [module.cluster_gke]
 }
-
-# #=============================================================================#
-# # Kong API-Gateway Configuration
-# module "kong_sql_instance" {
-#   source                      = "/home/cme/certi/terraform-modules/modules/cloud_sql/instance"
-
-#   project_id                  = var.project_id
-#   region                      = var.region
-#   instance_name               = "api-gw-database"
-
-#   database_version            = "POSTGRES_9_6"
-#   root_password               = var.sql_kong
-#   database_tier               = "db-f1-micro"
-#   database_disk_type          = "PD_HDD"
-
-#   depends_on                  = [module.network_config]
-# }
-
-# module "kong_sql_database" {
-#   source                      = "/home/cme/certi/terraform-modules/modules/cloud_sql/database"
-
-#   project_id                  = var.project_id
-#   database_instance           = module.kong_sql_instance.instance_name
-#   database_name               = "kong"
-
-#   depends_on                  = [module.kong_sql_instance]
-# }
-
-# module "konga_sql_database" {
-#   source                      = "/home/cme/certi/terraform-modules/modules/cloud_sql/database"
-
-#   project_id                  = var.project_id
-#   database_instance           = module.kong_sql_instance.instance_name
-#   database_name               = "konga"
-
-#   depends_on                  = [module.kong_sql_instance]
-# }
-
-# module "kong_sql_user" {
-#   source                      = "/home/cme/certi/terraform-modules/modules/cloud_sql/user"
-
-#   project_id                  = var.project_id
-#   database_instance           = module.kong_sql_instance.instance_name
-#   database_user               = "kong"
-#   user_password               = var.user_password_kong
-
-#   depends_on                  = [module.kong_sql_instance]
-# }
-
-# module "kong_compute_instance" {
-#   source                      = "/home/cme/certi/terraform-modules/modules/kong/"
-
-#   project_id                  = var.project_id
-#   region                      = var.region
-#   worker_ipv4_cidr_block      = var.worker_ipv4_cidr_block
-
-#   depends_on                  = [module.kong_sql_instance, module.kong_sql_user]
-# }
-
-# module "firewall_rules" {
-#   source                    = "/home/cme/certi/terraform-modules/modules/firewall"
-
-#   region                    = var.region
-#   project_id                = var.project_id
-#   source_ranges             = var.master_ipv4_cidr_block
-#   target_tags_name          = "certi-cluster"
-#   fw_rules_name             = "certi-istio"
-#   fw_allow_ports            = ["8080","10250","443","15017","9443"]
-# }
